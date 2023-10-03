2373b004997db86baff7e7098465587a96d97d30
Jeppe Sommer <jso@trifork.com>
2023-10-03 15:08:55 +0200

    Cleanup

--- impromptu-request-forwarder/src/main/java/com/trifork/impromptu/InternalDelegator.java
+++ impromptu-request-forwarder/src/main/java/com/trifork/impromptu/InternalDelegator.java
@@ -4,7 +4,6 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
 import java.util.Comparator;
 import java.util.Optional;
 
